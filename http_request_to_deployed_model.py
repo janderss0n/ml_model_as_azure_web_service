d9a269eaa4969eccfd668f992a8a760cfe1bcf62
Jessica Andersson <anderssonjessica92@gmail.com>
2019-01-28 15:21:14 +0100

    Refactor

--- http_request_to_deployed_model.py
+++ http_request_to_deployed_model.py
@@ -3,32 +3,59 @@ import json
 import pandas as pd
 import os
 
-uri = your_web_service_uri_here
 
-data = pd.read_csv('data/test.csv')
-X_pred = data.drop(columns=['SalePrice'])
-X_pred_list = X_pred.values.tolist()
+def get_data(path_to_prediction_data):
+    return pd.read_csv(path_to_prediction_data)
 
-# send a random row from the test set to score
-input_data = '{\"data\": ' + str(X_pred_list) + '}'
 
-headers = {'Content-Type':'application/json'}
+def drop_columns(data, columns_to_drop):
+    return data.drop(columns=columns_to_drop)
 
-# for AKS deployment you'd need to the service key in the header as well
-# api_key = service.get_key()
-# headers = {'Content-Type':'application/json',  'Authorization':('Bearer '+ api_key)}
 
-resp = requests.post(uri, input_data, headers=headers)
-pred_output = eval(eval(resp.text))
+def convert_data_to_json_format(X):
+    X_list = X.values.tolist()
+    return '{\"data\": ' + str(X_list) + '}'
 
-print('POST to url', uri)
-#print('input data:', input_data)
-#print('label:', data.SalePrice)
-#print('********')
-#print('prediction:', pred_output)
 
-os.makedirs('prediction', exist_ok=True)
-prediction = pd.DataFrame()
-prediction['actual_salePrice'] = data.SalePrice.copy()
-prediction['prediction_salePrice'] = pred_output
-prediction.to_csv('prediction/prediction_made_on_test.csv', index=False)
+def make_resquest(uri, input_data, headers):
+    return requests.post(uri, input_data, headers=headers)
+
+
+def filter_resp(resp):
+    return eval(eval(resp.text))
+
+
+def combine_actual_with_output(target_name, data, model_output, compare_output_to_actual):
+    prediction = pd.DataFrame()
+    if compare_output_to_actual:
+        prediction['actual_'+target_name] = data[target_name].copy()
+
+    prediction['prediction_'+target_name] = model_output
+    return prediction
+
+
+def save_model_output(prediction, folder, filename):
+    os.makedirs(folder, exist_ok=True)
+    prediction.to_csv(folder+'/'+filename, index=False)
+
+
+if __name__=='__main__':
+    uri = os.getenv('URI')
+    compare_output_to_actual = True
+    path_to_prediction_data = 'data/test.csv'
+    target_name = 'SalePrice'
+    columns_to_drop=[target_name]
+    headers = {'Content-Type':'application/json'}
+    model_output_folder = 'prediction'
+    model_output_filename = 'prediction_made_on_test.csv'
+    # for AKS deployment you'd need to the service key in the header as well
+    # api_key = service.get_key()
+    # headers = {'Content-Type':'application/json',  'Authorization':('Bearer '+ api_key)}
+
+    data = get_data(path_to_prediction_data)
+    X = drop_columns(data, columns_to_drop)
+    input_data = convert_data_to_json_format(X)
+    resp = make_resquest(uri, input_data, headers)
+    model_output = filter_resp(resp)
+    prediction = combine_actual_with_output(target_name, data, model_output, compare_output_to_actual)
+    save_model_output(prediction, model_output_folder, model_output_filename)
